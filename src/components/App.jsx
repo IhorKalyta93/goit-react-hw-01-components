8753f8a3d26050694ce10eb8b922304ad50ea4c9
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-02-14 01:06:59 +0100

    1.2.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,8 @@ import stat from "../data/data.json";
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 
-<TransactionHistory items={transactions} />;
-<FriendList friends={friends} />;
+{/* <TransactionHistory items={transactions} />;
+<FriendList friends={friends} />; */}
 
 
 
